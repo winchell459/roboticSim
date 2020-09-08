a8c5254c9dc6224c10fb3667f173c14951d19d9b
john morris <johncmorris@gmail.com>
2020-09-08 10:55:17 -0700

    end of day 1

---
+++ Assets/Scripts/TheRobotController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheRobotController : MonoBehaviour
+{
+    public float Torque = 5.0f; //float - > real numbers
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.GetAxis("Left") + "  " + Input.GetAxis("Right"));
+        float left = Input.GetAxis("Left");
+        float right = Input.GetAxis("Right");
+
+        float rotation = left - right;
+
+        //if(left > right) 
+
+    }
+}
