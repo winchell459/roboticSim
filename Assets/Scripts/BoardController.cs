cd24770d6b97d06531fccca7bbd016a718e6c14f
winchell459 <johncmorris@gmail.com>
2020-09-18 15:32:25 -0700

    end of 9/15/20

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    public GameObject HorizontalLinePrefab, VerticalLinePrefab;
+    public Vector3 LineStartOffset = new Vector3(0.1f, 0, 0.1f);
+    public float LineSpacing = 0.2f;
+    public float BoardScaleFactor = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setupBoard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void setupBoard()
+    {
+        Vector3 boardOrigin = transform.position;
+        Vector3 boardScale = transform.localScale;
+
+        boardOrigin -= BoardScaleFactor * boardScale / 2;
+        Vector3 boardEnd = boardOrigin + BoardScaleFactor * boardScale;
+
+        //place Horizontal lines
+        float currentZ = boardOrigin.z + LineStartOffset.z * BoardScaleFactor;
+        while(currentZ < boardEnd.z)
+        {
+            GameObject line = Instantiate(HorizontalLinePrefab);
+            line.transform.position = new Vector3(transform.position.x, 0, currentZ);
+            currentZ += LineSpacing * BoardScaleFactor;
+        }
+
+        //place Vertical lines
+        float currentX = boardOrigin.x + LineStartOffset.x * BoardScaleFactor;
+        while (currentX < boardEnd.x)
+        {
+            GameObject line = Instantiate(VerticalLinePrefab);
+            line.transform.position = new Vector3(currentX, 0, transform.position.z);
+            currentX += LineSpacing * BoardScaleFactor;
+        }
+    }
+}
--- Assets/Scripts/TheRobotController.cs
+++ Assets/Scripts/TheRobotController.cs
@@ -8,6 +8,9 @@ public class TheRobotController : MonoBehaviour
     public float Torque = 5.0f; //float - > real numbers
     private Rigidbody rb;
 
+    public LayerMask Mask;
+    public float RayDistance = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,7 @@ public class TheRobotController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.GetAxis("Left") + "  " + Input.GetAxis("Right"));
+        //Debug.Log(Input.GetAxis("Left") + "  " + Input.GetAxis("Right"));
         float moving = Input.GetAxis("Left");
         float turning = Input.GetAxis("Right"); // right = -1 full power on right wheels  -1 = 100% right 0 = 50% right and 1 = 0% right
 
@@ -33,7 +36,30 @@ public class TheRobotController : MonoBehaviour
         rb.velocity = velocity * transform.forward;
         transform.Rotate(rotation * transform.up * Torque);
 
-        //if(left > right) 
+        checkFork();
 
     }
+
+    private void checkFork()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(transform.position, transform.forward * RayDistance, Color.red);
+
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, RayDistance))
+        {
+            Vector3 displacement = hit.collider.transform.position - transform.position;
+            if(Vector3.Dot(displacement, rb.velocity) < 0) //less than zero means release the riser
+            {
+                hit.collider.transform.parent = null;
+                hit.collider.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            else if(hit.collider.transform.parent != transform)
+            {
+                hit.collider.transform.parent = transform;
+                hit.collider.GetComponent<Rigidbody>().isKinematic = true;
+            }
+        }
+    }
+    
 }
