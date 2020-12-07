5a42681bc0ccdb7c3d7877af7f6a346071454492
winchell459 <johncmorris@gmail.com>
2020-12-07 14:40:13 -0800

    end of day 12/6/20

--- Assets/Scripts/ScoringHandler.cs
+++ Assets/Scripts/ScoringHandler.cs
@@ -8,6 +8,8 @@ public class ScoringHandler : MonoBehaviour
     public ScoringZone G0_1, G1_1, G2_1;
     public ScoringZone G0_2, G1_2, G2_2;
 
+    private bool[,] inCompletedRow = new bool[,] { { false, false, false }, { false, false, false }, { false, false, false } };
+
     public ScoreCard Card;
 
     int numOfRisers, numOfRiserStacks;
@@ -18,27 +20,75 @@ public class ScoringHandler : MonoBehaviour
 
 
         //check rows
-        if (validSequence(G0_0.RiserStack[0], G1_0.RiserStack[0], G2_0.RiserStack[0])) score += 3;
-        if (validSequence(G0_1.RiserStack[0], G1_1.RiserStack[0], G2_1.RiserStack[0])) score += 3;
-        if (validSequence(G0_2.RiserStack[0], G1_2.RiserStack[0], G2_2.RiserStack[0])) score += 3;
+        if (validSequence(G0_0.RiserStack[0], G1_0.RiserStack[0], G2_0.RiserStack[0]))
+        {
+            score += 3;
+            inCompletedRow[0, 0] = true;
+            inCompletedRow[1, 0] = true;
+            inCompletedRow[2, 0] = true;
+        }
+        if (validSequence(G0_1.RiserStack[0], G1_1.RiserStack[0], G2_1.RiserStack[0]))
+        {
+            score += 3;
+            inCompletedRow[0, 1] = true;
+            inCompletedRow[1, 1] = true;
+            inCompletedRow[2, 1] = true;
+        }
+        if (validSequence(G0_2.RiserStack[0], G1_2.RiserStack[0], G2_2.RiserStack[0]))
+        {
+            score += 3;
+            inCompletedRow[0, 2] = true;
+            inCompletedRow[1, 2] = true;
+            inCompletedRow[2, 2] = true;
+        }
         //check cols
-        if (validSequence(G0_0.RiserStack[0], G0_1.RiserStack[0], G0_2.RiserStack[0])) score += 3;
-        if (validSequence(G1_0.RiserStack[0], G1_1.RiserStack[0], G1_2.RiserStack[0])) score += 3;
-        if (validSequence(G2_0.RiserStack[0], G2_1.RiserStack[0], G2_2.RiserStack[0])) score += 3;
+        if (validSequence(G0_0.RiserStack[0], G0_1.RiserStack[0], G0_2.RiserStack[0]))
+        {
+            score += 3;
+            inCompletedRow[0, 0] = true;
+            inCompletedRow[0, 1] = true;
+            inCompletedRow[0, 2] = true;
+        }
+        if (validSequence(G1_0.RiserStack[0], G1_1.RiserStack[0], G1_2.RiserStack[0]))
+        {
+            score += 3;
+            inCompletedRow[1, 0] = true;
+            inCompletedRow[1, 1] = true;
+            inCompletedRow[1, 2] = true;
+        }
+        if (validSequence(G2_0.RiserStack[0], G2_1.RiserStack[0], G2_2.RiserStack[0]))
+        {
+            score += 3;
+            inCompletedRow[2, 0] = true;
+            inCompletedRow[2, 1] = true;
+            inCompletedRow[2, 2] = true;
+        }
         //check diagonals
-        if (validSequence(G0_0.RiserStack[0], G1_1.RiserStack[0], G2_2.RiserStack[0])) score += 3;
-        if (validSequence(G2_0.RiserStack[0], G1_1.RiserStack[0], G0_2.RiserStack[0])) score += 3;
+        if (validSequence(G0_0.RiserStack[0], G1_1.RiserStack[0], G2_2.RiserStack[0]))
+        {
+            score += 3;
+            inCompletedRow[0, 0] = true;
+            inCompletedRow[1, 1] = true;
+            inCompletedRow[2, 2] = true;
+        }
+        if (validSequence(G2_0.RiserStack[0], G1_1.RiserStack[0], G0_2.RiserStack[0]))
+        {
+            score += 3;
+            inCompletedRow[2, 0] = true;
+            inCompletedRow[1, 1] = true;
+            inCompletedRow[0, 2] = true;
+        }
 
-        
-        score += checkStack(G0_0.RiserStack);
-        score += checkStack(G1_0.RiserStack);
-        score += checkStack(G2_0.RiserStack);
-        score += checkStack(G0_1.RiserStack);
-        score += checkStack(G1_1.RiserStack);
-        score += checkStack(G2_1.RiserStack);
-        score += checkStack(G0_2.RiserStack);
-        score += checkStack(G1_2.RiserStack);
-        score += checkStack(G2_2.RiserStack);
+
+        score += checkStack(G0_0.RiserStack, inCompletedRow[0, 0]);
+        score += checkStack(G1_0.RiserStack, inCompletedRow[1, 0]);
+        score += checkStack(G2_0.RiserStack, inCompletedRow[2, 0]);
+        score += checkStack(G0_1.RiserStack, inCompletedRow[0, 1]);
+        score += checkStack(G1_1.RiserStack, inCompletedRow[1, 1]);
+        score += checkStack(G2_1.RiserStack, inCompletedRow[2, 1]);
+        score += checkStack(G0_2.RiserStack, inCompletedRow[0, 2]);
+        score += checkStack(G1_2.RiserStack, inCompletedRow[1, 2]);
+        score += checkStack(G2_2.RiserStack, inCompletedRow[2, 2]);
 
         Riser.RiserColors[] selections = new Riser.RiserColors[9];
         // selections = {0_0, 0_1, 0_2, 1_0, 1_1, 1_2, 2_0, 2_1, 2_2};
@@ -90,7 +140,7 @@ public class ScoringHandler : MonoBehaviour
         else return false;
     }
 
-    private int checkStack(List<Riser> stack)
+    private int checkStack(List<Riser> stack, bool hasCompletedRow)
     {
         int score = 0;
         if (validPlacement(stack[0]))
@@ -109,7 +159,7 @@ public class ScoringHandler : MonoBehaviour
                     score += 1; // another point for the top riser
                     numOfRisers += 1;
                     Debug.Log("+1 triple stack " + stack[0].RiserColor);
-                    if (stack[0].RiserColor == stack[1].RiserColor && stack[2].RiserColor == stack[1].RiserColor)
+                    if (hasCompletedRow && stack[0].RiserColor == stack[1].RiserColor && stack[2].RiserColor == stack[1].RiserColor)
                     {
                         score += 30; //30 points for having a stack of 3 of the same color
                         numOfRiserStacks += 1;
